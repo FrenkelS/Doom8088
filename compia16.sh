1b8825b9c69011213e225cd0bc3c397d69cce295
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-11-20 23:58:36 +0100

    render options

--- compia16.sh
+++ compia16.sh
@@ -2,6 +2,7 @@ mkdir GCCIA16
 
 unset CFLAGS
 
+#export RENDER_OPTIONS="-DONE_WALL_TEXTURE -DFLAT_WALL -DFLAT_SPAN -DFLAT_SKY -DDISABLE_STATUS_BAR"
 export RENDER_OPTIONS="-DFLAT_SPAN"
 
 ia16-elf-gcc -c i_system.c $RENDER_OPTIONS -march=i286 -mcmodel=medium -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops
