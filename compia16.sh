ec2789a9d37828d47d44a3fd1637bd28554c4736
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2023-12-19 09:24:09 +0100

    move visplane code to r_plane.c

--- compia16.sh
+++ compia16.sh
@@ -7,6 +7,7 @@ export RENDER_OPTIONS="-DFLAT_SPAN"
 
 ia16-elf-gcc -c i_system.c $RENDER_OPTIONS -march=i286 -mcmodel=medium -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops
 ia16-elf-gcc -c r_draw.c   $RENDER_OPTIONS -march=i286 -mcmodel=medium -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops
+ia16-elf-gcc -c r_plane.c  $RENDER_OPTIONS -march=i286 -mcmodel=medium -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops
 ia16-elf-gcc -c tables.c   $RENDER_OPTIONS -march=i286 -mcmodel=medium -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops
 
 export CFLAGS="-march=i286 -mcmodel=medium -li86 -Os -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops"
@@ -52,7 +53,8 @@ export GLOBOBJS+=" p_user.c"
 export GLOBOBJS+=" r_data.c"
 #export GLOBOBJS+=" r_draw.c"
 export GLOBOBJS+=" r_draw.o"
-export GLOBOBJS+=" r_plane.c"
+#export GLOBOBJS+=" r_plane.c"
+export GLOBOBJS+=" r_plane.o"
 export GLOBOBJS+=" r_sky.c"
 export GLOBOBJS+=" r_things.c"
 export GLOBOBJS+=" s_sound.c"
@@ -70,4 +72,5 @@ ia16-elf-gcc $GLOBOBJS $CFLAGS $RENDER_OPTIONS -o GCCIA16/DOOM8088.EXE
 
 rm i_system.o
 rm r_draw.o
+rm r_plane.o
 rm tables.o
