a0d2543a9bab1af6c040de8d541a737240e70237
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-01-03 18:48:20 +0100

    Copyright 2023, 2024

--- doomdef.h
+++ doomdef.h
@@ -10,7 +10,7 @@
  *  Jess Haas, Nicolas Kalkhof, Colin Phipps, Florian Schulze
  *  Copyright 2005, 2006 by
  *  Florian Schulze, Colin Phipps, Neil Stevens, Andrey Budko
- *  Copyright 2023 by
+ *  Copyright 2023, 2024 by
  *  Frenkel Smeijers
  *
  *  This program is free software; you can redistribute it and/or
