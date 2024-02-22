5c99915dad49b4863d1e31ba2fb5431f1b734785
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-02-22 18:09:45 +0100

    optimize r_plane for speed

--- compiamy.sh
+++ compiamy.sh
@@ -13,6 +13,7 @@ ia16-elf-gcc -c p_map.c    $RENDER_OPTIONS -march=i286 -mcmodel=medium -mnewlib-
 ia16-elf-gcc -c p_maputl.c $RENDER_OPTIONS -march=i286 -mcmodel=medium -mnewlib-nano-stdio -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops -freorder-blocks-algorithm=simple
 ia16-elf-gcc -c p_sight.c  $RENDER_OPTIONS -march=i286 -mcmodel=medium -mnewlib-nano-stdio -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops -freorder-blocks-algorithm=simple
 ia16-elf-gcc -c r_draw.c   $RENDER_OPTIONS -march=i286 -mcmodel=medium -mnewlib-nano-stdio -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops -freorder-blocks-algorithm=simple
+ia16-elf-gcc -c r_plane.c  $RENDER_OPTIONS -march=i286 -mcmodel=medium -mnewlib-nano-stdio -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops -freorder-blocks-algorithm=simple
 ia16-elf-gcc -c tables.c   $RENDER_OPTIONS -march=i286 -mcmodel=medium -mnewlib-nano-stdio -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops -freorder-blocks-algorithm=simple
 ia16-elf-gcc -c w_wad.c    $RENDER_OPTIONS -march=i286 -mcmodel=medium -mnewlib-nano-stdio -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops -freorder-blocks-algorithm=simple
 ia16-elf-gcc -c z_zone.c   $RENDER_OPTIONS -march=i286 -mcmodel=medium -mnewlib-nano-stdio -Ofast -fomit-frame-pointer -fgcse-sm -fgcse-las -fipa-pta -mregparmcall -flto -fwhole-program -funroll-loops -freorder-blocks-algorithm=simple
@@ -63,7 +64,8 @@ export GLOBOBJS+=" p_user.c"
 export GLOBOBJS+=" r_data.c"
 #export GLOBOBJS+=" r_draw.c"
 export GLOBOBJS+=" r_draw.o"
-export GLOBOBJS+=" r_plane.c"
+#export GLOBOBJS+=" r_plane.c"
+export GLOBOBJS+=" r_plane.o"
 export GLOBOBJS+=" r_sky.c"
 export GLOBOBJS+=" r_things.c"
 export GLOBOBJS+=" s_sound.c"
@@ -88,6 +90,7 @@ rm p_map.o
 rm p_maputl.o
 rm p_sight.o
 rm r_draw.o
+rm r_plane.o
 rm tables.o
 rm w_wad.o
 rm z_zone.o
