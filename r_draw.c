4839c17a09c667bbcb9e2db5634ab8b85fb3be5e
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-04-28 21:57:02 +0200

    IPSPRITEYSCALE -> PSPRITEYISCALE

--- r_draw.c
+++ r_draw.c
@@ -260,7 +260,7 @@ static const int16_t PSPRITESCALE  = FRACUNIT * VIEWWINDOWWIDTH / SCREENWIDTH_VG
 static const fixed_t PSPRITEISCALE = FRACUNIT * SCREENWIDTH_VGA / VIEWWINDOWWIDTH; // = FixedReciprocal(PSPRITESCALE)
 
 static const uint16_t PSPRITEYSCALE = FRACUNIT * (VIEWWINDOWHEIGHT * 5 / 4) / SCREENHEIGHT_VGA;
-static const fixed_t IPSPRITEYSCALE = FRACUNIT * SCREENHEIGHT_VGA / (VIEWWINDOWHEIGHT * 5 / 4); // = FixedReciprocal(PSPRITEYSCALE)
+static const fixed_t PSPRITEYISCALE = FRACUNIT * SCREENHEIGHT_VGA / (VIEWWINDOWHEIGHT * 5 / 4); // = FixedReciprocal(PSPRITEYSCALE)
 
 static const angle16_t clipangle = 0x2008; // = xtoviewangleTable[0]
 
@@ -1132,7 +1132,7 @@ static void R_DrawPSprite (pspdef_t *psp, int16_t lightlevel)
     vis->x2 = x2 >= VIEWWINDOWWIDTH ? VIEWWINDOWWIDTH - 1 : x2;
     // proff 11/06/98: Added for high-res
     vis->scale = PSPRITEYSCALE;
-    vis->iscale = IPSPRITEYSCALE;
+    vis->iscale = PSPRITEYISCALE;
 
     vis->xiscale = PSPRITEISCALE;
     vis->startfrac = 0;
