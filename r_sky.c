bc831cf87da8418ab9c77b17ba16b231a8618c46
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-05-22 11:08:35 +0200

    Copyright

--- r_sky.c
+++ r_sky.c
@@ -1,7 +1,7 @@
 /*-----------------------------------------------------------------------------
  *
  *
- *  Copyright (C) 2023 - 2024 Frenkel Smeijers
+ *  Copyright (C) 2023-2024 Frenkel Smeijers
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
