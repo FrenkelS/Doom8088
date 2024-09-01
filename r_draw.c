3c5cfa088976b0de6ee5d5c0107d171aea9fef27
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-09-01 17:44:16 +0200

    replace qsort with insertion sort, since the keys are roughly in order to begin with, due to BSP rendering.

--- r_draw.c
+++ r_draw.c
@@ -1144,35 +1144,38 @@ static void R_DrawPlayerSprites(void)
 //
 // R_SortVisSprites
 //
-static int compare (const void* l, const void* r)
-{
-	const vissprite_t* vl = *(const vissprite_t**)l;
-	const vissprite_t* vr = *(const vissprite_t**)r;
 
-	fixed_t diff = vr->scale - vl->scale;
-	if (!diff)
-		return 0;
-	else if (diff < 0)
-		return -1;
-	else
-		return 1;
+// insertion sort
+static void isort(vissprite_t **s, int16_t n)
+{
+	for (int16_t i = 1; i < n; i++)
+	{
+		vissprite_t *temp = s[i];
+		if (s[i - 1]->scale < temp->scale)
+		{
+			int16_t j = i;
+			while ((s[j] = s[j - 1])->scale < temp->scale && --j)
+				;
+			s[j] = temp;
+		}
+	}
 }
 
 #define MAXVISSPRITES 96
-static int8_t num_vissprite;
+static int16_t num_vissprite;
 static vissprite_t vissprites[MAXVISSPRITES];
 static vissprite_t* vissprite_ptrs[MAXVISSPRITES];
 
 static void R_SortVisSprites (void)
 {
-    int8_t i = num_vissprite;
+    int16_t i = num_vissprite;
 
     if (i)
     {
         while (--i >= 0)
             vissprite_ptrs[i] = vissprites + i;
 
-        qsort(vissprite_ptrs, num_vissprite, sizeof (vissprite_t*), compare);
+        isort(vissprite_ptrs, num_vissprite);
     }
 }
 
@@ -1189,7 +1192,7 @@ static void R_DrawMasked(void)
     R_SortVisSprites();
 
     // draw all vissprites back to front
-    for (int8_t i = num_vissprite; --i >= 0; )
+    for (int16_t i = num_vissprite; --i >= 0; )
         R_DrawSprite(vissprite_ptrs[i]);
 
     // render any remaining masked mid textures
