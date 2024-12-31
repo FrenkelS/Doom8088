374b597412a2bd6fe1225d3db1af2aa37b95b6ff
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-12-31 17:58:31 +0100

    R_DrawColumnFlat in assembly

--- i_vmodey.c
+++ i_vmodey.c
@@ -341,6 +341,60 @@ void R_DrawColumnWall(const draw_column_vars_t *dcvars)
 }
 
 
+#if defined C_ONLY
+static void R_DrawColumnFlat2(uint8_t color, uint8_t dontcare, int16_t count)
+{
+	UNUSED(dontcare);
+
+	int16_t l = count >> 4;
+
+	while (l--)
+	{
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+		*dest = color; dest += PLANEWIDTH;
+	}
+	
+	switch (count & 15)
+	{
+		case 15: dest[PLANEWIDTH * 14] = color;
+		case 14: dest[PLANEWIDTH * 13] = color;
+		case 13: dest[PLANEWIDTH * 12] = color;
+		case 12: dest[PLANEWIDTH * 11] = color;
+		case 11: dest[PLANEWIDTH * 10] = color;
+		case 10: dest[PLANEWIDTH *  9] = color;
+		case  9: dest[PLANEWIDTH *  8] = color;
+		case  8: dest[PLANEWIDTH *  7] = color;
+		case  7: dest[PLANEWIDTH *  6] = color;
+		case  6: dest[PLANEWIDTH *  5] = color;
+		case  5: dest[PLANEWIDTH *  4] = color;
+		case  4: dest[PLANEWIDTH *  3] = color;
+		case  3: dest[PLANEWIDTH *  2] = color;
+		case  2: dest[PLANEWIDTH *  1] = color;
+		case  1: dest[PLANEWIDTH *  0] = color;
+	}
+}
+#else
+void R_DrawColumnFlat2(uint8_t color, uint8_t dontcare, int16_t count);
+#endif
+
+
 void R_DrawColumnFlat(uint8_t color, const draw_column_vars_t *dcvars)
 {
 	int16_t count = (dcvars->yh - dcvars->yl) + 1;
@@ -349,13 +403,9 @@ void R_DrawColumnFlat(uint8_t color, const draw_column_vars_t *dcvars)
 	if (count <= 0)
 		return;
 
-	uint8_t __far* dest = _s_screen + (dcvars->yl * PLANEWIDTH) + dcvars->x;
+	dest = _s_screen + (dcvars->yl * PLANEWIDTH) + dcvars->x;
 
-	while (count--)
-	{
-		*dest = color;
-		dest += PLANEWIDTH;
-	}
+	R_DrawColumnFlat2(color, color, count);
 }
 
 
